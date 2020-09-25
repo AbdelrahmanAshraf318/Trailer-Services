4d201b6ffb7c52056861b1c6734d16ba09564849
Abdelrahman Ashraf <bodyronaldo25@gmail.com>
2020-09-25 19:05:20 +0200

    Trailer-Website

---
+++ main.js
@@ -0,0 +1,152 @@
+$(document).ready(function() {
+	// body...
+	$('#services').on('click' , function() {
+
+		
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#Servi').show();
+		$('#background').css('background-image', 'url(trailer.jpg)');
+		$('#news').hide();
+		$('#faqs').hide();
+		$('#aboutUs').hide();
+		$('#back').hide();
+		$('#contact').hide();
+		
+
+	});
+});
+
+$(document).ready(function() {
+	// body...
+	$('#NEWS').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('#faqs').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').show();
+		$('#background').css('background-image', 'url(trailer.jpg)');
+		$('#aboutUs').hide();
+		$('#back').hide();
+		$('#contact').hide();
+		
+
+	});
+});
+
+$(document).ready(function() {
+	// body...
+	$('#FAQS').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').hide();
+		$('#faqs').show();
+		$('#back').show();
+		$('#aboutUs').hide();
+		$('#contact').hide();
+		
+
+	});
+});
+$(document).ready(function() {
+	// body...
+	$('#about').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').hide();
+		$('#faqs').hide();
+		$('#back').show();
+		$('#aboutUs').show();
+		$('#contact').hide();
+	});
+});
+$(document).ready(function() {
+	// body...
+	$('#abo').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').hide();
+		$('#faqs').hide();
+		$('#back').show();
+		$('#aboutUs').show();
+		$('#contact').hide();
+	});
+});
+
+$(document).ready(function() {
+	// body...
+	$('#con2').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').hide();
+		$('#faqs').hide();
+		$('#back').show();
+		$('#aboutUs').hide();
+		$('#contact').show();
+	});
+});
+$(document).ready(function() {
+	// body...
+	$('#con').on('click' , function() {
+
+		$('.interface').hide();
+		$('.paragraph').hide();
+		$('.about').hide();
+		$('.paragraph2').hide();
+		$('.news').hide();
+		$('#Servi').hide();
+		$('.contact').hide();
+		$('.contact2').show();
+		$('.footer').show();
+		$('#news').hide();
+		$('#faqs').hide();
+		$('#back').show();
+		$('#aboutUs').hide();
+		$('#contact').show();
+	});
+});
